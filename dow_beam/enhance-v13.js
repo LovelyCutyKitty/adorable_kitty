1dd439032b124a1e0f0b8c68361f4f538f9ecee4
Love_me_more <90360923+LovelyCutyKitty@users.noreply.github.com>
2026-08-13 21:11:11 +0900

    Refresh inventory taxonomy script cache

--- dow_beam/enhance-v13.js
+++ dow_beam/enhance-v13.js
@@ -47,4 +47,4 @@ const editScript=document.createElement('script');editScript.async=false;editScr
 
 const groupingScript=document.createElement('script');groupingScript.async=false;groupingScript.src='enhance-v17.js?rev=1';document.body.append(groupingScript);
 
-const taxonomyScript=document.createElement('script');taxonomyScript.async=false;taxonomyScript.src='enhance-v18.js?rev=1';document.body.append(taxonomyScript);
+const taxonomyScript=document.createElement('script');taxonomyScript.async=false;taxonomyScript.src='enhance-v18.js?rev=2';document.body.append(taxonomyScript);
