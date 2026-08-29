fcc3b2f16d525305eceaa000152457343be7f4c6
Love_me_more <90360923+LovelyCutyKitty@users.noreply.github.com>
2026-08-30 01:30:18 +0900

    Refresh construction material cache for UI update

--- construction_material/sw.js
+++ construction_material/sw.js
@@ -1,6 +1,9 @@
-
-const CACHE="construction-material-cm-20260830-1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./data/meta.js","./data/questions_01.js","./data/questions_02.js","./data/questions_03.js","./data/questions_04.js","./data/questions_05.js","./data/questions_06.js","./data/questions_07.js","./data/questions_08.js","./data/finalize.js"];
+const CACHE="construction-material-cm-20260830-2";
+const ASSETS=[
+  "./","./index.html","./styles.css","./ui-patch.css","./app.js","./manifest.webmanifest",
+  "./data/meta.js","./data/questions_01.js","./data/questions_02.js","./data/questions_03.js","./data/questions_04.js","./data/questions_05.js","./data/questions_06.js","./data/questions_07.js","./data/questions_08.js","./data/questions_09.js","./data/questions_10.js","./data/questions_11.js","./data/finalize.js",
+  "./code_01.js","./code_02.js","./code_03.js","./code_04.js","./code_05.js"
+];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
