5fa40915e9267378a057ce3cf05c9731f5682480
Love_me_more <90360923+LovelyCutyKitty@users.noreply.github.com>
2026-09-11 18:17:02 +0900

    Refresh material app cache for answer reveal

--- construction_material/sw.js
+++ construction_material/sw.js
@@ -1,8 +1,8 @@
-const CACHE="construction-material-cm-20260830-10";
+const CACHE="construction-material-cm-20260830-13";
 const ASSETS=[
   "./","./index.html","./styles.css","./ui-patch.css","../final-ux.css","./app.js","./latex-patch.js","../final-ux.js","./manifest.webmanifest",
   "./data/meta.js","./data/questions_01.js","./data/questions_02.js","./data/questions_03.js","./data/questions_04.js","./data/questions_05.js","./data/questions_06.js","./data/questions_07.js","./data/questions_08.js","./data/questions_09.js","./data/questions_10.js","./data/questions_11.js","./data/finalize.js",
-  "./code_01.js","./code_02.js","./code_03.js","./code_04.js","./code_05.js"
+  "./code_01.js","./code_02.js","./code_03.js","./code_04.js","./code_05.js","./code_06.js"
 ];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
